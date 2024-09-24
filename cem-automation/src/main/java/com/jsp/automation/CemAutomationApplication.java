d814d7ad5edbb92b74c433135a1d454574598f7a
rakeshpothal <rakeshpothal3@gmail.com>
2024-09-24 12:06:47 +0530

    automation project

---
+++ cem-automation/src/main/java/com/jsp/automation/CemAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemAutomationApplication.class, args);
+	}
+
+}
